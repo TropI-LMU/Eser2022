b8ea147f1999fded130cf41ec51fadde953d6c89
olgabaranov <olga.baranov@posteo.de>
2023-12-18 10:30:16 +0100

    fixed bugs connected to missing col name for rowlabels

--- Ziegler/01_DataPreprocessing.R
+++ Ziegler/01_DataPreprocessing.R
@@ -7,8 +7,8 @@ args <- commandArgs(trailingOnly = TRUE)
 basedir <- args[1]
 codedir <- args[2]
 
-# basedir <- "/home/obaranov/projects/"
-# codedir = "/home/obaranov/projects/Tabea_Paper/code/"
+# basedir <- "/home/obaranov/projects/scRNA_newPaper/"
+# codedir = "/home/obaranov/projects/scRNA_newPaper//code/"
 
 setwd(basedir)
 
@@ -18,7 +18,9 @@ source(paste0(codedir, "/utility/General_Utility.R"))
 # source(paste0(basedir, "FunctionArchive/Annotate_Pathways.R"))
 # source(paste0(basedir,"/Tabea_Paper/code/utility/General_Utility.R"))
 
-commonlabs = vroom(paste0(codedir,"/utility/CellTypeMapping.csv"))
+commonlabs <- vroom(paste0(codedir, "/utility/CellTypeMapping.csv"))
+
+# this way of reading it is not working FIX IT
 
 f <- file("RawData/20210220_NasalSwab_NormCounts.txt")
 cols <- readLines(f, n = 1)
@@ -32,13 +34,12 @@ r.cols <- unlist(strsplit(cols, "\t"))
 
 r.counts <- vroom("RawData/20210220_NasalSwab_RawCounts.txt",
     skip = 2,
-    col_names = r.cols
+    col_names = c("GeneSymbol", r.cols)
 )
 
-
-meta <- vroom("RawData/20210701_NasalSwab_MetaData.txt") %>% 
-            dplyr::slice(-1) %>% 
-            column_to_rownames('NAME')
+meta <- vroom("RawData/20210701_NasalSwab_MetaData.txt") %>%
+    dplyr::slice(-1) %>%
+    column_to_rownames("NAME")
 umap <- vroom("RawData/20210220_NasalSwab_UMAP.txt") %>% dplyr::slice(-1)
 
 fullziegler <- CreateSeuratObject(r.counts %>% column_to_rownames("GeneSymbol"),
@@ -51,9 +52,9 @@ fullziegler[["percent.mt"]] <- PercentageFeatureSet(fullziegler, pattern = "^MT-
 fullziegler <- subset(fullziegler, subset = nCount_RNA < 20000 & nCount_RNA > 1000 & percent.mt < 20)
 
 # add custom annotation
-fullziegler@meta.data = makeCommonAnno(commonlabs, fullziegler[[]], "Ziegler", "Detailed_Cell_Annotations")
+fullziegler@meta.data <- makeCommonAnno(commonlabs, fullziegler[[]], "Ziegler", "Detailed_Cell_Annotations")
 
-zieglerno19 = subset(fullziegler, donor_id != "COVID19_Participant19")
+zieglerno19 <- subset(fullziegler, donor_id != "COVID19_Participant19")
 saveRDS(zieglerno19, "FilteredData/no19_seurat.rds")
 
 Tcells <- subset(zieglerno19, Coarse_Cell_Annotations == "T Cells")
@@ -67,8 +68,12 @@ cases <- meta %>%
     rownames()
 
 r.counts <- vroom("RawData/20210220_NasalSwab_RawCounts.txt",
-    col_select = all_of(c("GeneSymbol", cases))
+    skip = 2,
+    col_names = c("GeneSymbol", r.cols)
 )
+
+r.counts <- r.counts[c("GeneSymbol", cases)]
+
 
 covidpos <- CreateSeuratObject(r.counts %>% column_to_rownames("GeneSymbol"),
     min.cells = 3, min.genes = 10,
